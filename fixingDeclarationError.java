2aeb98b84e0bce3825561499fea103ca5a710ab9
unknown <holupkern@gmail.com>
2023-11-01 17:02:11 +0800

    fixing declaration error of types for variables

---
+++ fixingDeclarationError.java
@@ -0,0 +1,13 @@
+public class fixingDeclarationError {
+    public static void main(String[] args) {
+        int year = 2001;
+        String title = "Shrek";
+        char genre = 'C';
+        double runtime = 1.58;
+        boolean isPG = true;
+
+        System.out.println(
+                "Year = " + year + "\ntitle = " + title + "\ngenre = " + genre + "\nruntime = " + runtime + "\nisPG = "
+                        + isPG);
+    }
+}
---
+++ playChar.java
@@ -0,0 +1,15 @@
+public class playChar {
+    public static void main(String[] args) {
+        char expectedGrade = 'A';
+        String stringHere = new String("Hello World");
+        String escapedStringHere = "\"meow\"";
+        String escapedLineBreak = "Hello there, \nObiwan";
+        System.out.println(expectedGrade);
+        System.out.println(stringHere);
+        System.out.println(escapedStringHere);
+        System.out.print(escapedLineBreak);
+    }
+}
+
+// playing with char, for characters.;
+// '' is differentiated from string "";
