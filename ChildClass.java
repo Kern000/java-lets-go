3bb24fe9d1ee1256cb72811bded3a2cbcc3c2098
unknown <holupkern@gmail.com>
2023-11-06 17:39:24 +0800

    playing with polymorphism

--- ChildClass.java
+++ ChildClass.java
@@ -1,8 +1,27 @@
-class ChildClass extends ParentClass {
+public class ChildClass extends ParentClass {
 
-    ChildClass() {
-        super(30.0, 0.64, "flat", "rice flour");
+    ChildClass(double arg1, double arg2, String arg3, String arg4) {
+        super(arg1, arg2, arg3, arg4);
 
+    }
+
+    // attempt to method override
+    @Override
+    protected boolean isTasty() {
+        return false;
+    }
+
+    // thus the child is able to use both its own override method and the parent
+    // class's own method with same name
+
+    public void whetherNoodleIsTasty() {
+        super.isTasty();
+    }
+
+    // Polymorphism, defining child object as a parent class object
+    public void createRamen() {
+        ParentClass superRamen = new ChildClass(10.0, 0.15, "round", "wheat flour");
+        System.out.println(superRamen.shape);
     }
 
 }
--- ParentClass.java
+++ ParentClass.java
@@ -3,11 +3,11 @@
 class ParentClass {
 
     // instance variables w/o static
-    double lengthCentimeters;
-    double widthCentimeters;
-    String shape;
-    String ingredients;
-    String texture = "raw";
+    protected double lengthCentimeters;
+    protected double widthCentimeters;
+    protected String shape;
+    protected String ingredients;
+    protected String texture = "raw";
 
     // constructor in parent class
     protected ParentClass(double lengthInCM, double widthInCM, String shapeForm, String cookingIngredients) {
@@ -18,14 +18,35 @@ class ParentClass {
     }
 
     // optional instance method
-    public void cook() {
+    protected final String cook() {
         this.texture = "cooked al dente";
+        return this.texture;
     }
 
-    // entry point of prog
+    // if put final, the childClass cannot method override by using same name
+    protected boolean isTasty() {
+        return true;
+    }
+
+    // can violate SOLID's open-closed principle if say, modify parent code to add
+    // new conditional statements
+    protected final boolean isMadeWithHeart() {
+        return true;
+    }
+
+    // testing polymorphism methods
+    protected final String makeIntoMainDish(ParentClass noodle) {
+        return "This is " + noodle.cook();
+    }
+
+    // entry point of prog. Currently this violates Dependency inversion, by
+    // coupling the parent and child class together
     public static void main(String[] args) {
-        ChildClass spaghettiPomodoro = new ChildClass();
+        ChildClass spaghettiPomodoro = new ChildClass(15.0, 3.15, "flat", "semolina flour");
         System.out.println(spaghettiPomodoro.shape);
+        System.out.println("Is this tasty? " + spaghettiPomodoro.isTasty());
+        System.out.println("Made with heart?" + spaghettiPomodoro.isMadeWithHeart());
+        System.out.println(spaghettiPomodoro.makeIntoMainDish(spaghettiPomodoro));
 
     }
 }
