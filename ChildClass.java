2738dbad9107cc74809573bf45a731e15ae77897
unknown <holupkern@gmail.com>
2023-11-06 16:17:31 +0800

    extends, child inherit constructor thru supepr(), and initializing child class instance in ParentClass

--- ChildClass.java
+++ ChildClass.java
@@ -1,3 +1,8 @@
 class ChildClass extends ParentClass {
 
+    ChildClass() {
+        super(30.0, 0.64, "flat", "rice flour");
+
+    }
+
 }
--- ParentClass.java
+++ ParentClass.java
@@ -4,17 +4,28 @@ class ParentClass {
 
     // instance variables w/o static
     double lengthCentimeters;
+    double widthCentimeters;
     String shape;
-    String texture = "smooth";
+    String ingredients;
+    String texture = "raw";
+
+    // constructor in parent class
+    protected ParentClass(double lengthInCM, double widthInCM, String shapeForm, String cookingIngredients) {
+        this.lengthCentimeters = lengthInCM;
+        this.widthCentimeters = widthInCM;
+        this.shape = shapeForm;
+        this.ingredients = cookingIngredients;
+    }
 
     // optional instance method
     public void cook() {
-        this.texture = "cooked";
+        this.texture = "cooked al dente";
     }
 
     // entry point of prog
     public static void main(String[] args) {
         ChildClass spaghettiPomodoro = new ChildClass();
-        System.out.println(spaghettiPomodoro.texture);
+        System.out.println(spaghettiPomodoro.shape);
+
     }
 }
